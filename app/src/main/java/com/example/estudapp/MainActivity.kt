e186c10dacd9d9fb467dcba730c610e498a7ac76
Chloe Scaramal <skaramal7@gmail.com>
2025-09-11 11:44:41 -0300

    Main Activity setup

--- app/src/main/java/com/example/estudapp/MainActivity.kt
+++ app/src/main/java/com/example/estudapp/MainActivity.kt
@@ -4,44 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.estudapp.navigate.EPPNavHost
+import com.example.estudapp.ui.feature.auth.AuthViewModel
 import com.example.estudapp.ui.theme.EstudaTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val authViewModel : AuthViewModel by viewModels()
         setContent {
-            EstudaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Box(
+                modifier = Modifier
+                    .safeDrawingPadding()
+            ) {
+                EstudaTheme {
+                    EPPNavHost(authViewModel = authViewModel)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EstudaTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/estudapp/ui/feature/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.example.estudapp.ui.feature.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen() {
+    
+}
